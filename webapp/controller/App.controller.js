d156be3896f87f2459d9a71a491d3ab97376b356
İbrahim Halil SAKAR <ibrahimhalilsakar0@gmail.com>
2022-05-19 02:21:49 +0300

    19.05.2022

--- webapp/controller/App.controller.js
+++ webapp/controller/App.controller.js
@@ -1,23 +1,36 @@
 sap.ui.define([
-    'sap/ui/core/mvc/Controller',
-    'sap/m/MessageToast',
-    'sap/ui/model/json/JSONModel'
-], function(Controller, MessageToast, JSONModel) {
-    'use strict';
-    return Controller.extend("sap.ui.demo.walktrough.App", {
-        onShowHello : function () {
-            // alert("Hello there!!!");
-            MessageToast.show("Hello There");
-        },
-        onInit : function(){
-            var oData = {
-                recipeint : {
-                    name: "UI5"
+	'sap/ui/core/mvc/Controller',
+	'sap/m/MessageToast',
+	'sap/ui/model/json/JSONModel',
+	'sap/ui/model/resource/ResourceModel'
+], function(Controller, MessageToast, JSONModel, ResourceModel) {
+	'use strict';
+	return Controller.extend("demo.controller.App", {
 
-                }
-            };
-            var oModel = new JSONModel(oData);
-            this.getView().setModel(oModel);
-        }
-    })
-}); 
+		onInit : function () {
+			//set data model on view
+			var oData = {
+				recipient : {
+					name : "World"
+				}
+			}
+			var oModel = new JSONModel(oData);
+			this.getView().setModel(oModel);
+
+			//set i18n model on view
+			var i18nModel = new ResourceModel({
+				bundleName: "demo.i18n.i18n"
+			});
+			this.getView().setModel(i18nModel, "i18n");
+		},
+
+		onShowHello : function () {
+			//Read msg from i18n model
+			var oBundle = this.getView().getModel("i18n").getResourceBundle();
+			var sRecipient = this.getView().getModel().getProperty("/recipient/name");
+			var sMsg = oBundle.getText("helloMsg", [sRecipient]);
+			//show msg
+			MessageToast.show(sMsg);
+		}
+	});
+});
--- webapp/index.js
+++ webapp/index.js
@@ -1,13 +1,17 @@
 sap.ui.define([
-    "sap/ui/core/mvc/XMLView"
-    
-], function (XMLView) {
-	"use strict";
+	"sap/m/Text",
+	"sap/ui/core/mvc/XMLView"
+], function(Text, XMLView) {
+	'use strict';
+	// alert("UI5 is ready!");
 
 	XMLView.create({
-		viewName: "sap.ui.demo.walkthrough.view.App"
+		viewName: "demo.view.App"
 	}).then(function (oView) {
 		oView.placeAt("content");
-	});
+	})
 
+	// new Text({
+	// 	text: "Hello World!"
+	// }).placeAt("content");
 });
